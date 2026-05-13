6e50cde3ea55eed475ead1a6f6ce5bd145aea5e8
memmelma <marius.memmel@gmx.net>
2026-05-13 13:32:18 -0700

    add local training config

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1063,7 +1063,7 @@ _CONFIGS = [
         wandb_enabled=False,
     ),
 
-    # weighted BC training
+    # weighted BC training - tillicum
     TrainConfig(
         name="weighted_bc_tillicum",
         model=pi0_config.Pi0Config(pi05=True, paligemma_variant="gemma_2b_lora"),
@@ -1088,6 +1088,26 @@ _CONFIGS = [
         ).get_freeze_filter(),
         ema_decay=None,
     ),
+    # weighted BC training - local
+    TrainConfig(
+        name="weighted_bc_local",
+        model=pi0_config.Pi0Config(pi05=True, paligemma_variant="gemma_2b_lora"),
+        data=LeRobotYAMDataConfig(
+            repo_id="memmelma/block_bowl",
+            base_config=DataConfig(prompt_from_task=True, reward_name="rvlm_max_reward"),
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "gs://openpi-assets/checkpoints/pi05_base/params"
+        ),
+        num_train_steps=30_000,
+        batch_size=4,
+        num_workers=8,
+        freeze_filter=pi0_config.Pi0Config(
+            pi05=True, paligemma_variant="gemma_2b_lora"
+        ).get_freeze_filter(),
+        ema_decay=None,
+    ),
+
     # RoboArena & PolaRiS configs.
     *roboarena_config.get_roboarena_configs(),
     *polaris_config.get_polaris_configs(),
