62a9698ba96d34d8786b441ac8e6c54b6e3ad76d
memmelma <marius.memmel@gmx.net>
2026-05-13 13:36:28 -0700

    make sure normalization exists

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+$CONFIG="weighted_bc_tillicum"
+
+uv run scripts/compute_norm_stats.py --config-name=$CONFIG
+
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py \
-    weighted_bc_tillicum \
+    $CONFIG \
       --exp-name debug_run_1 \
       --num-train-steps 15000 \
       --overwrite
